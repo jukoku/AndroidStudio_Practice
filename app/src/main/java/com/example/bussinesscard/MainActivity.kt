7229447d199d206657274f8ec25a800567a0070a
jinho <jukoku@gmail.com>
2025-04-29 23:40:10 +0900

    bussiness card main part

--- app/src/main/java/com/example/bussinesscard/MainActivity.kt
+++ app/src/main/java/com/example/bussinesscard/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bussinesscard.ui.theme.BussinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +41,37 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Composable
+fun Mainpart(){
+    val image = painterResource(R.drawable.android_logo)
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color(0xFF6BA987)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Box(Modifier.padding(start = 120.dp, end = 120.dp, bottom = 10.dp)
+            .background(Color(0xFF122E04))) {
+            Image(
+                painter = image,
+                contentDescription = null
+            )
+        }
+        Text(
+            text = "John Doe",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(5.dp)
+        )
+        Text(
+            text = "Android Developer"
+        )
+    }
+}
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -42,6 +84,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BussinessCardTheme {
-        Greeting("Android")
+        Mainpart()
     }
 }
