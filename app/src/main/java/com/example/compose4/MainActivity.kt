0eda35057e21cf64d83cc6dc2043462fad4a3dfe
jinho <jukoku@gmail.com>
2025-04-29 18:32:37 +0900

    remain some problems

--- app/src/main/java/com/example/compose4/MainActivity.kt
+++ app/src/main/java/com/example/compose4/MainActivity.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose4.ui.theme.Compose4Theme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +38,15 @@ class MainActivity : ComponentActivity() {
                     //import androidx.compose.material3.MaterialTheme
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text_composable(
-                        title = "Text composable\n",
-                        context = "Displays text and follows the recommended Material Design guidelines.\n"
+                    Four_composable(
+                        titleText = "Text composable\n",
+                        contextText = "Displays text and follows the recommended Material Design guidelines.\n",
+                        titleImage = "Image composable",
+                        contextImage = "Creates a composable that lays out and draws a given Painter class object.\n",
+                        titleRow = "Row composable\n",
+                        contextRow = "A layout composable that places its children in a horizontal sequence.\n",
+                        titleColumn = "Column composable\n",
+                        contextColumn = "A layout composable that places its children in a vertical sequence.\n"
                     )
                 }
             }
@@ -40,17 +54,135 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun Text_composable(title: String, context: String, modifier: Modifier = Modifier) {
+fun Four_composable(
+    titleText: String, titleImage: String,
+    titleRow: String, titleColumn: String,
+    contextText: String, contextImage: String,
+    contextRow: String, contextColumn: String,
+    modifier: Modifier = Modifier
+    ){
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Row (
+            modifier = modifier
+                .weight(2f)
+        ) {
+            Text_composable(
+                titleText = titleText,
+                contextText = contextText,
+                modifier = modifier
+                    .weight(1f)
+                    .background(color = Color(0xFFEADDFF))
+            )
+            Image_composable(
+                titleImage = titleImage,
+                contextImage = contextImage,
+                modifier = modifier
+                    .weight(1f)
+                    .background(color = Color(0xFFD0BCFF))
+            )
+        }
+        Row (
+            modifier = modifier
+                .weight(2f)
+        ) {
+            Row_composable(
+                titleRow = titleRow,
+                contextRow = contextRow,
+                modifier = modifier
+                    .weight(1f)
+                    .background(color = Color(0xFFB69DF8))
+            )
+            Column_composable(
+                titleColumn = titleColumn,
+                contextColumn = contextColumn,
+                modifier = modifier
+                    .weight(1f)
+                    .background(color = Color(0xFFF6EDFF))
+            )
+        }
+    }
+}
+
+@Composable
+fun Text_composable(titleText: String, contextText: String, modifier: Modifier = Modifier) {
     Column(
-        
+        verticalArrangement= Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
     ) {
         Text(
-            text = title,
+            text = titleText,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
             modifier = modifier
         )
         Text(
-            text = context,
+            text = contextText,
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun Image_composable(titleImage: String, contextImage: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement= Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = titleImage,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+        )
+        Text(
+            text = contextImage,
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun Row_composable(titleRow: String, contextRow: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement= Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = titleRow,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+        )
+        Text(
+            text = contextRow,
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun Column_composable(titleColumn: String, contextColumn: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement= Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = titleColumn,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+        )
+        Text(
+            text = contextColumn,
             textAlign = TextAlign.Justify,
             modifier = modifier
         )
@@ -61,9 +193,15 @@ fun Text_composable(title: String, context: String, modifier: Modifier = Modifie
 @Composable
 fun GreetingPreview() {
     Compose4Theme {
-        Text_composable(
-            title = "Text composable\n",
-            context = "Displays text and follows the recommended Material Design guidelines.\n"
+        Four_composable(
+            titleText = "Text composable\n",
+            contextText = "Displays text and follows the recommended Material Design guidelines.\n",
+            titleImage = "Image composable",
+            contextImage = "Creates a composable that lays out and draws a given Painter class object.\n",
+            titleRow = "Row composable\n",
+            contextRow = "A layout composable that places its children in a horizontal sequence.\n",
+            titleColumn = "Column composable\n",
+            contextColumn = "A layout composable that places its children in a vertical sequence.\n"
         )
     }
 }
