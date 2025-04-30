d13fd2e3ce925fe8d099db4d6d5e0a73ac6477a3
jinho <jukoku@gmail.com>
2025-04-30 12:09:09 +0900

    Lazy Column

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -21,6 +21,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -48,14 +50,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp2(
     modifier: Modifier = Modifier,
-    names:List<String> = listOf("World", "Compose", "Zero", "Import")
-    ){
-    Column(modifier) {
-        for (name in names){
+    names:List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             AddButton(name = name)
         }
     }
-
 }
 
 @Composable
