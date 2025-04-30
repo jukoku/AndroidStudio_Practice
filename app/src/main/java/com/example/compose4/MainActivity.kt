d8667a8cfc3827bf09c772069ef46005b8c55f0c
jinho <jukoku@gmail.com>
2025-04-30 09:21:35 +0900

    finally tested

--- app/src/main/java/com/example/compose4/MainActivity.kt
+++ app/src/main/java/com/example/compose4/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,8 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,153 +37,65 @@ class MainActivity : ComponentActivity() {
                     //import androidx.compose.material3.MaterialTheme
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Four_composable(
-                        titleText = "Text composable\n",
-                        contextText = "Displays text and follows the recommended Material Design guidelines.\n",
-                        titleImage = "Image composable",
-                        contextImage = "Creates a composable that lays out and draws a given Painter class object.\n",
-                        titleRow = "Row composable\n",
-                        contextRow = "A layout composable that places its children in a horizontal sequence.\n",
-                        titleColumn = "Column composable\n",
-                        contextColumn = "A layout composable that places its children in a vertical sequence.\n"
-                    )
+                    QuadrupleComposable()
                 }
             }
         }
     }
 }
 
+@Composable
+fun QuadrupleComposable(){
+    Column(Modifier.fillMaxWidth()){
+        Row(Modifier.weight(1f)){
+            OneOfQuadruple(
+                backgroundColor = Color(0xFFEADDFF),
+                title = stringResource(R.string.text_composable),
+                description = stringResource(R.string.displays_text_and_follows_the_recommended_material_design_guidelines),
+                modifier = Modifier.weight(1f)
+            )
+            OneOfQuadruple(
+                backgroundColor = Color(0xFFD0BCFF),
+                title = stringResource(R.string.image_composable),
+                description = stringResource(R.string.creates_a_composable_that_lays_out_and_draws_a_given_painter_class_object),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight((1f))){
+            OneOfQuadruple(
+                backgroundColor = Color(0xFFB69DF8),
+                title = stringResource(R.string.row_composable),
+                description = stringResource(R.string.a_layout_composable_that_places_its_children_in_a_horizontal_sequence),
+                modifier = Modifier.weight(1f)
+            )
+            OneOfQuadruple(
+                backgroundColor = Color(0xFFF6EDFF),
+                title = stringResource(R.string.column_composable),
+                description = stringResource(R.string.a_layout_composable_that_places_its_children_in_a_vertical_sequence),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
 
 @Composable
-fun Four_composable(
-    titleText: String, titleImage: String,
-    titleRow: String, titleColumn: String,
-    contextText: String, contextImage: String,
-    contextRow: String, contextColumn: String,
-    modifier: Modifier = Modifier
-    ){
-    Column (
+fun OneOfQuadruple(backgroundColor: Color, title: String, description: String, modifier: Modifier = Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
+            .background(backgroundColor)
             .fillMaxSize()
-    ) {
-        Row (
-            modifier = modifier
-                .weight(2f)
-        ) {
-            Text_composable(
-                titleText = titleText,
-                contextText = contextText,
-                modifier = modifier
-                    .weight(1f)
-                    .background(color = Color(0xFFEADDFF))
-            )
-            Image_composable(
-                titleImage = titleImage,
-                contextImage = contextImage,
-                modifier = modifier
-                    .weight(1f)
-                    .background(color = Color(0xFFD0BCFF))
-            )
-        }
-        Row (
-            modifier = modifier
-                .weight(2f)
-        ) {
-            Row_composable(
-                titleRow = titleRow,
-                contextRow = contextRow,
-                modifier = modifier
-                    .weight(1f)
-                    .background(color = Color(0xFFB69DF8))
-            )
-            Column_composable(
-                titleColumn = titleColumn,
-                contextColumn = contextColumn,
-                modifier = modifier
-                    .weight(1f)
-                    .background(color = Color(0xFFF6EDFF))
-            )
-        }
-    }
-}
-
-@Composable
-fun Text_composable(titleText: String, contextText: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement= Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-    ) {
+            .padding(16.dp)
+    ){
         Text(
-            text = titleText,
-            textAlign = TextAlign.Center,
+            text = title,
             fontWeight = FontWeight.Bold,
-            modifier = modifier
+            modifier = Modifier.padding(bottom = 5.dp)
         )
         Text(
-            text = contextText,
-            textAlign = TextAlign.Justify,
-            modifier = modifier
-        )
-    }
-}
-
-@Composable
-fun Image_composable(titleImage: String, contextImage: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement= Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-    ) {
-        Text(
-            text = titleImage,
-            fontWeight = FontWeight.Bold,
-            modifier = modifier
-        )
-        Text(
-            text = contextImage,
-            textAlign = TextAlign.Justify,
-            modifier = modifier
-        )
-    }
-}
-
-@Composable
-fun Row_composable(titleRow: String, contextRow: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement= Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-    ) {
-        Text(
-            text = titleRow,
-            fontWeight = FontWeight.Bold,
-            modifier = modifier
-        )
-        Text(
-            text = contextRow,
-            textAlign = TextAlign.Justify,
-            modifier = modifier
-        )
-    }
-}
-
-@Composable
-fun Column_composable(titleColumn: String, contextColumn: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement= Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-    ) {
-        Text(
-            text = titleColumn,
-            fontWeight = FontWeight.Bold,
-            modifier = modifier
-        )
-        Text(
-            text = contextColumn,
-            textAlign = TextAlign.Justify,
-            modifier = modifier
+            text = description,
+            textAlign = TextAlign.Justify
         )
     }
 }
@@ -193,15 +104,6 @@ fun Column_composable(titleColumn: String, contextColumn: String, modifier: Modi
 @Composable
 fun GreetingPreview() {
     Compose4Theme {
-        Four_composable(
-            titleText = "Text composable\n",
-            contextText = "Displays text and follows the recommended Material Design guidelines.\n",
-            titleImage = "Image composable",
-            contextImage = "Creates a composable that lays out and draws a given Painter class object.\n",
-            titleRow = "Row composable\n",
-            contextRow = "A layout composable that places its children in a horizontal sequence.\n",
-            titleColumn = "Column composable\n",
-            contextColumn = "A layout composable that places its children in a vertical sequence.\n"
-        )
+        QuadrupleComposable()
     }
 }
