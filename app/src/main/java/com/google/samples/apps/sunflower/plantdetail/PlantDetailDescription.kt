cff8f86e1f6f905b411281d864bd9011237dd866
jinho <jukoku@gmail.com>
2025-05-02 11:56:22 +0900

    당근 예약

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,11 +16,14 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,7 +40,11 @@ fun PlantDetailDescription() {
 fun PlantName(name: String){
     Text(
         text = name,
-        modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally),
+        style = MaterialTheme.typography.headlineSmall
     )
 }
 
