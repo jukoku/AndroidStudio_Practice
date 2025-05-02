b1993abb020ae8ca8c6fa39591d183e90abb936c
jinho <jukoku@gmail.com>
2025-05-02 11:48:13 +0900

    Migration start

---
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.plantdetail
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.sunflower.R
+
+@Composable
+fun PlantDetailDescription() {
+    Surface {
+        Text("Hello Compose")
+    }
+}
+
+@Composable
+fun PlantName(name: String){
+    Text(
+        text = name,
+        modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun PlantNamePreview(){
+    MaterialTheme {
+        PlantName(name = "Avocado")
+    }
+}
