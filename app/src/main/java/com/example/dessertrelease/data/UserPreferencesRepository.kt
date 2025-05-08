6ef8e5dc59eee7d45e8332836698a27f235614f9
jinho <jukoku@gmail.com>
2025-05-08 10:57:00 +0900

    DataStore Starter Dessert

---
+++ app/src/main/java/com/example/dessertrelease/data/UserPreferencesRepository.kt
@@ -0,0 +1,39 @@
+package com.example.dessertrelease.data
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class UserPreferencesRepository(
+    private val dataStore: DataStore<Preferences>
+) {
+    private companion object{
+        val IS_LINEA_LAYOUT = booleanPreferencesKey("is_liner_layout")
+        const val TAG = "UserPreferenceRepo"
+    }
+
+    suspend fun saveLayoutPreference(isLinerLayout: Boolean){
+        dataStore.edit { preferences ->
+            preferences[IS_LINEA_LAYOUT] = isLinerLayout
+        }
+    }
+
+    val isLinerLayout: Flow<Boolean> = dataStore.data
+        .catch {
+            if(it is IOException){
+                Log.e(TAG, "Error reading preferences", it)
+            } else {
+                throw it
+            }
+        }
+        .map{ preferences ->
+        preferences[IS_LINEA_LAYOUT] ?: true
+    }
+
+}
